6660c8162a83bce084c5c0d8074879c4156e1f65
Dev1 <isyoon@tapasmedia.co>
2015-07-14 15:40:24 -0700

    Adjust ga code.

--- resources/js/ga.js
+++ resources/js/ga.js
@@ -3,8 +3,7 @@
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-34830778-1', 'tapastic.com');
-ga('require', 'displayfeatures');
+ga('create', 'UA-34830778-1', 'auto');
 ga('send', {
   'hitType': 'pageview',
   'page': '/index',
